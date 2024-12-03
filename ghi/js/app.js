f4710cd0333170c03fdbe62ac11e0f21a4d17471
Aurora <stephanie.a.pio@gmail.com>
2024-12-03 19:00:56 +0100

    location form

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -22,7 +22,6 @@ window.addEventListener('DOMContentLoaded', async () => {
     var alertHere = document.getElementById('alert')
 
     function alert(message) {
-        console.log(message)
         var wrapper = document.createElement('div')
         wrapper.innerHTML = '<div class="alert alert-dark">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
 
